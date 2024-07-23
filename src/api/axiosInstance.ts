6d7c357309cd971df549f9176e9d6d31d675e0e1
ankitgupta-13 <guptankit0522@gmail.com>
2024-07-23 15:44:58 +0530

    fix:axiosInstance

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -5,7 +5,6 @@ const baseURL = import.meta.env.VITE_BACKEND_URL;
 export const api = axios.create({
   baseURL: baseURL,
   headers: {
-    authorization: `Bearer ${document.cookie.split(";")}`,
     Accept: "application/json",
     "Content-Type": "application/json",
   },
